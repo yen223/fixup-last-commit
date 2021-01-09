931fa0b758f480b7a96af3a88d67d6aabc813e45
Wei Yen <hello@weiyen.net>
2021-01-09 19:02:57 +1100

    Initial commit

---
+++ git-fixup-last-commit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# For each file that has changed, create a fixup! commit for the last commit which changed that file
+# This is useful when you've committed a bunch of work across multiple files,
+# and you want to run an autoformatter, but you don't want to create a commit just for fixing formatting
+
+git diff --name-only | while read filename;
+do
+  commit_sha=$(git log --pretty=format:"%H" $filename | head -n 1);
+  git add "$filename" && git commit -m "fixup! $commit_sha"
+done
